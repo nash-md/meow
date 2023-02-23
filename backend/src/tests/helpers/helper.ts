0922f4791f2a9189c1014890a4f6f13bc4da05f0
nash-md <polygon.puma@gmail.com>
2023-02-23 10:10:00 +0100

    added integration test helper

---
+++ backend/src/tests/helpers/helper.ts
@@ -0,0 +1,65 @@
+import request from 'supertest';
+import { ANIMALS } from '../../Constants.js';
+import { Lane } from '../../entities/Lane.js';
+
+function generateRandomString(length: number): string {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+const createRandomUser = () => {
+  return {
+    name: `${ANIMALS[1]}_${generateRandomString(6)}`,
+    password: generateRandomString(12),
+    id: '',
+    accountId: '',
+  };
+};
+
+const createRandomUserWithToken = async (url: string) => {
+  const context = {
+    token: '',
+    user: Helper.createRandomUser(),
+    lanes: <Lane[]>[],
+  };
+
+  await request(url)
+    .post('/public/register')
+    .set('Content-Type', 'application/json')
+    .send({
+      name: context.user.name,
+      password: context.user.password,
+    });
+
+  const res = await request(url)
+    .post('/public/login')
+    .set('Content-Type', 'application/json')
+    .send({
+      name: context.user.name,
+      password: context.user.password,
+    });
+
+  context.token = res.body.token;
+  context.user.id = res.body.user.id;
+  context.user.accountId = res.body.account.id;
+
+  const lanesRequest = await request(url)
+    .get('/api/lanes')
+    .set('Content-Type', 'application/json')
+    .set('Token', context.token);
+
+  context.lanes = lanesRequest.body;
+
+  return context;
+};
+
+export const Helper = {
+  generateRandomString,
+  createRandomUser,
+  createRandomUserWithToken,
+};
