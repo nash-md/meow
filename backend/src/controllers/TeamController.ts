2b5755404438f134bf5fa40c821f7ebd9fc13314
nash-md <polygon.puma@gmail.com>
2023-04-15 22:35:25 +0200

    Added integrations to team controller

--- backend/src/controllers/TeamController.ts
+++ backend/src/controllers/TeamController.ts
@@ -3,7 +3,7 @@ import { CurrencyCode, Team } from '../entities/Team.js';
 import { InvalidRequestBodyError } from '../errors/InvalidRequestBodyError.js';
 import { EntityHelper } from '../helpers/EntityHelper.js';
 import { AuthenticatedRequest } from '../requests/AuthenticatedRequest.js';
-import { database } from '../worker.js';
+import { datasource } from '../helpers/DatabaseHelper.js';
 
 const parseCurrencyCode = (value: string): CurrencyCode => {
   if (value in CurrencyCode) {
@@ -27,7 +27,7 @@ const update = async (
 
       team.currency = parseCurrencyCode(req.body.currency);
 
-      const updated = await database.manager.save(team);
+      const updated = await datasource.manager.save(team);
 
       return res.json(updated);
     }
@@ -36,6 +36,60 @@ const update = async (
   }
 };
 
+const updateIntegration = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (req.params.id) {
+      const team = await EntityHelper.findOneById(
+        req.jwt.user,
+        Team,
+        req.params.id
+      );
+
+      const integrations = team.integrations ?? [];
+
+      const updatedIntegrations = integrations.filter(
+        (integration) => integration.key !== req.body.key
+      );
+
+      updatedIntegrations.push(req.body);
+
+      team.integrations = updatedIntegrations;
+
+      const updated = await datasource.manager.save(team);
+
+      return res.json(updated);
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const get = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (req.params.id) {
+      const team = await EntityHelper.findOneById(
+        req.jwt.user,
+        Team,
+        req.params.id
+      );
+
+      return res.json(team);
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+
 export const TeamController = {
   update,
+  updateIntegration,
+  get,
 };
