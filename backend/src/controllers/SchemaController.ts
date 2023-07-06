61dd87d7e98b5df62c063e639d472125889d9d41
nash-md <polygon.puma@gmail.com>
2023-07-06 20:17:28 +0200

    Support for schema filtering via query param

--- backend/src/controllers/SchemaController.ts
+++ backend/src/controllers/SchemaController.ts
@@ -4,27 +4,41 @@ import { InvalidSchemaPropertyError } from '../errors/InvalidSchemaPropertyError
 import { EntityHelper } from '../helpers/EntityHelper.js';
 import { AuthenticatedRequest } from '../requests/AuthenticatedRequest.js';
 import { datasource } from '../helpers/DatabaseHelper.js';
+import { InvalidRequestQueryParameterError } from '../errors/InvalidRequestQueryParameterError.js';
 
 function parseSchemaType(value: unknown): SchemaType {
   switch (value) {
-    case 'card':
-      return SchemaType.Card;
     case 'account':
       return SchemaType.Account;
+    case 'card':
+      return SchemaType.Card;
     default:
-      throw new InvalidSchemaPropertyError(
-        `Unsupported status value: ${value}`
-      );
+      throw new InvalidSchemaPropertyError(`Unsupported type value: ${value}`);
   }
 }
 
-const list = async (
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction
-) => {
+function parseTypeFromRequest(value: unknown): SchemaType {
+  switch (value) {
+    case 'account':
+      return SchemaType.Account;
+    case 'card':
+      return SchemaType.Card;
+    default:
+      throw new InvalidRequestQueryParameterError(`Unsupported type ${value}`);
+  }
+}
+
+const list = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   try {
     const schemas = await EntityHelper.findByTeam(Schema, req.jwt.team);
+
+    if (req.query.type) {
+      const type = parseTypeFromRequest(req.query.type);
+
+      return res.json(schemas.filter((schema) => schema.type === type));
+    } else {
+      return res.json(schemas);
+    }
 
     return res.json(schemas);
   } catch (error) {
@@ -32,11 +46,7 @@ const list = async (
   }
 };
 
-const create = async (
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction
-) => {
+const create = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   try {
     const query = {
       where: {
@@ -52,14 +62,10 @@ const create = async (
     }
 
     const updated = await datasource.manager.save(
-      new Schema(
-        req.jwt.team.id?.toString()!,
-        parseSchemaType(req.body.type),
-        req.body.schema
-      )
+      new Schema(req.jwt.team.id?.toString()!, parseSchemaType(req.body.type), req.body.schema)
     );
 
-    return res.json(updated);
+    return res.status(201).json(updated);
   } catch (error) {
     return next(error);
   }
